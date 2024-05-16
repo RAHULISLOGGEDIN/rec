dc00b1de67c4fd23e583479f32caa3801ef29ae4
rahul g <rahulissignedin@outlook.com>
2024-05-17 04:36:54 +0530

    nesting of routes

--- src/reactRouterDom/App.js
+++ src/reactRouterDom/App.js
@@ -8,16 +8,23 @@ import NotFound from "./pages/NotFound";
 import Blogs from "./pages/Blogs";
 import StopwatchParent from "./pages/stopwatch/StopwatchParent";
 import Crypto from "./pages/crypto_proj/App";
+import Temp from "./pages/Temp";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<div>This is new about</div>} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/blogs/:id/:numId" element={<Blogs />} />
-        <Route path="/blogs" element={<Blogs />} />
+
+        <Route path="/blogs">
+          <Route index element={<Temp />} />
+          <Route path=":id" element={<Blogs />} />
+          {/* <Route element={<Blogs />} /> */}
+          <Route path="category/:categoryId" element={<Blogs />} />
+        </Route>
+
         <Route path="/stopwatch" element={<StopwatchParent />} />
         <Route path="/crypto" element={<Crypto />} />
         <Route path="*" element={<NotFound />} />
